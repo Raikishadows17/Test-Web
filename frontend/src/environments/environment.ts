35731b1a61d12c38001444509b4c3ef2dfeaa76d
Gerardo Valente <gvacerox11@gmail.com>
2026-03-04 12:03:17 -0600

    Update apiURL in environment configuration

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
   production: true,
-  apiURL: "https://gale-cope-gary-dangerous.trycloudflare.com"
+  apiURL: "https://networking-con-insider-enable.trycloudflare.com/"
 };
