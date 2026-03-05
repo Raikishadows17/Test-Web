b52b8b5225e2d5c80a92662db46e53540487216e
Gerardo Valente <gvacerox11@gmail.com>
2026-03-05 11:15:32 -0600

    Update apiURL in environment configuration

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
   production: true,
-  apiURL: "https://networking-con-insider-enable.trycloudflare.com"
+  apiURL: "https://argue-symbol-neo-andrea.trycloudflare.com"
 };
